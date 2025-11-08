9fb27245d3187f04620650f57d4aacc39f26b030
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-08 23:49:11 +0530

    redirecting requests

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http=require("http");
+const fs=require("fs");
 const server=http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
     // process.exit();
+    const method=req.method;
     const url=req.url;
     if(url==='/'){
     res.write('<html>')
     res.write('<header><title>My first page</title></header>');
-    res.write('<body>form action="/message"method="POST"<input type="text"><button type="submit">Send</button></input></body>')
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>')
     res.write('</html>')
-    res.end();
+    return res.end();
+    }
+
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync("message.txt","DUMMY");
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
     }
     res.setHeader("Content-Type", "text/html");
     res.write('<html>')
