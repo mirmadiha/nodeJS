6f64755868143213e042cada377c164203199949
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-10 23:59:21 +0530

    NMS

--- app.js
+++ app.js
@@ -1,40 +1,6 @@
 const http=require("http");
-const fs=require("fs");
-const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
-    // process.exit();
-    const method=req.method;
-    const url=req.url;
-    if(url==='/'){
-    res.write('<html>')
-    res.write('<header><title>My first page</title></header>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end();
-    }
 
-    if(url==='/message' && method==='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message=parsedBody.split('=')[1];
-            fs.writeFile("message.txt",message,err=>{
-                res.statusCode=302;
-                res.setHeader('Location','/');
-                return res.end();
-            });
-        });
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write('<html>')
-    res.write('<header><title>My first page</title></header>');
-    res.write('<body><h1>hello from my nodejs!</h1></body>')
-    res.write('</html>')
-    res.end();
-});
+const routes=require("./routes");  //imports routes.js
+
+const server=http.createServer(routes);
 server.listen(3000);
