54940cedbec41c3f71fe473a3299f0bbce6d743d
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-16 20:19:18 +0530

    using Express Router

--- app.js
+++ app.js
@@ -6,27 +6,24 @@ const bodyParser=require("body-parser");
 
 const app=express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
 app.use((req,res,next)=>{
-    console.log("this will always run");
-    next();
-});
-
-app.use("/add-product",(req,res,next)=>{
-    console.log("in  another middleware!");
-    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Submit</button></form>');
-});
-
- app.use("/product",(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+    res.status(404).send("<h1>Page not found</h1>");
+})
 
  app.use("/",(req,res,next)=>{
     console.log("in the middleware");
     res.send("<h1>This is homepage</h1>")
 });
+
 
 const server=http.createServer(app);
 server.listen(4000);
