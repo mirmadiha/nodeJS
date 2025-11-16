5cb24a3fa4babcfb16eea2595275c73199b91335
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-16 17:03:29 +0530

    Parsing incoming requests

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const http=require("http");
 
 const express=require("express");
 
+const bodyParser=require("body-parser");
+
 const app=express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use((req,res,next)=>{
-    console.log("in the middleware!");
+    console.log("this will always run");
     next();
 });
 
-app.use((req,res,next)=>{
+app.use("/add-product",(req,res,next)=>{
     console.log("in  another middleware!");
-    //res.setHeader() : manually setting header
-    res.send("Hello from node js");
+    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Submit</button></form>');
+});
+
+ app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+ app.use("/",(req,res,next)=>{
+    console.log("in the middleware");
+    res.send("<h1>This is homepage</h1>")
 });
 
 const server=http.createServer(app);
