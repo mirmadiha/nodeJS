47745a49e53f9a0892258cdcacd5a343bf1bfbc6
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-22 22:51:39 +0530

    Adding layout to handlebars

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app=express();
 
 const expressHbs=require('express-handlebars');
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set("view engine","hbs");
 app.set('views','views');
 
