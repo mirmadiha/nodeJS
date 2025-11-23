5c2a09531057f6b17fd90fde816c73324268f751
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-23 21:02:38 +0530

    working with ejs

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const bodyParser=require("body-parser");
 
 const app=express();
 
-const expressHbs=require('express-handlebars');
-
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set("view engine","hbs");
+app.set("view engine","ejs");
 app.set('views','views');
 
 const adminData=require('./routes/admin');
