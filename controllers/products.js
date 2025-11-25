4c89d5f357370bad127840077ece9a7ddc81fe6d
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-25 23:52:54 +0530

    working on the navigation

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product=require('../models/product')
 
 exports.getAddProduct=((req,res,next)=>{
-    res.render('add-product', {pageTitle: 'Add Product',path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true })
+    res.render('admin/add-product', {pageTitle: 'Add Product',path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true })
 });
 
 exports.postAddProduct=((req,res,next)=>{
@@ -12,7 +12,7 @@ exports.postAddProduct=((req,res,next)=>{
 
 exports.getProducts=((req,res,next)=>{
     Product.fetchAll((products)=>{
-        res.render('shop',{prods:products, pageTitle:'Shop',path: '/', hasProducts: products.length>0, activeShop: true, productCSS: true});
+        res.render('shop/product-list',{prods:products, pageTitle:'Shop',path: '/', hasProducts: products.length>0, activeShop: true, productCSS: true});
     });
     
 }); 
