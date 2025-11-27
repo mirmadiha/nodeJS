4ba215b08445bdbd411f38bb4d53cf259f29c539
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-28 01:45:13 +0530

    displaying product data

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,17 @@
 const express=require("express");
 
-const productsController=require('../controllers/products')
+const shopController=require('../controllers/shop')
+const adminController=require('../controllers/admin')
 
 const router=express.Router();
 
-router.get("/add-product",productsController.getAddProduct);
+// admin/add-product => GET
+router.get("/add-product",adminController.getAddProduct);
 
- router.post("/add-product",productsController.postAddProduct);
+//admin/products =>GET
+router.get("/products",adminController.getProducts);
+
+// admin/add-product => POST
+router.post("/add-product",adminController.postAddProduct);
 
 module.exports=router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,17 @@ const express=require("express");
 
 const path=require("path");
 
-const productsController=require('../controllers/products')
+const shopController=require('../controllers/shop')
+const adminController=require('../controllers/shop')
 
 const router=express.Router();
 
-router.get('/',productsController.getProducts);
+router.get('/',shopController.getIndex);
+
+router.get('/cart',shopController.getCart);
+
+router.get('/products',shopController.getProducts);
+
+router.get('/checkout',shopController.getCheckout);
 
 module.exports=router
