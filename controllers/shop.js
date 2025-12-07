260e820f2eecd605fe342710d5afaddbef5f4423
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-12-07 22:39:30 +0530

    rendering product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,8 +18,7 @@ const Product=require('../models/product')
 exports.getProduct=((req,res,next)=>{
     const prodId=req.params.productId;
     Product.findById(prodId,product=>{
-        console.log(product);
-        res.redirect('/');
+        res.render('shop/product-detail',{product:product, pageTitle:product.title, path: '/products'});
     })
 })
 
