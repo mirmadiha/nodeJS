d773cb51dd259da999e67f25d41fc1dd55240404
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-12-08 16:41:46 +0530

    passing data with post request

--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,8 @@ router.get('/',shopController.getIndex);
 
 router.get('/cart',shopController.getCart);
 
+router.post('/cart',shopController.postCart);
+
 router.get('/products',shopController.getProducts);
 
 router.get('/products/:productId',shopController.getProduct);
