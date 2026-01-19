b10ab0294dc4fc30dcc4d023fa8638f6022a2e70
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-01-19 23:57:38 +0530

    defining a model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool= mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database:'node-complete',
-    password: 'Madiha@123'
+const sequelize = new Sequalize('node-complete', 'root', 'Madiha@123', {
+    dialect:'mysql', 
+    host:'localhost'
 });
 
-module.exports=pool.promise();
+module.exports = sequelize;
