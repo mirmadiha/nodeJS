2c9e326e71fcd5cf31272a06179b129b9609578c
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-01-22 00:00:06 +0530

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,14 +18,18 @@ exports.postEditProduct=((req,res,next)=>{
     const updatedPrice=req.body.price;
     const updatedImageUrl=req.body.imageUrl;
     const updatedDesc=req.body.description;
-    const updatedProduct=new Product(
-        prodId, 
-        updatedTitle, 
-        updatedImageUrl, 
-        updatedDesc, 
-        updatedPrice 
-    );
-    updatedProduct.save();
+    Product.findByPk(prodId)
+    .then(product=>{
+        product.title = updatedTitle;
+        product.price = updatedPrice;
+        product.imageUrl = updatedImageUrl;
+        product.description = updatedDesc;
+        return product.save();
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+
     res.redirect('/admin/products');
 })
 
@@ -69,8 +73,9 @@ exports.getEditProduct=((req,res,next)=>{
         return res.redirect('/products');
     }
     const prodId=req.params.productId;
-    Product.findById(prodId,product=>{
-        if(!product){
+    Product.findByPk(prodId)
+    .then(product=>{
+       if(!product){
             return res.redirect('/');
         }
         res.render('admin/edit-product',
@@ -78,7 +83,10 @@ exports.getEditProduct=((req,res,next)=>{
             path: '/admin/edit-product',
             editing:editMode,
             product:product
-        })
-    });
+        }) 
+    })
+    .catch(err=>{
+        console.log(err);
+    })
  });
 
