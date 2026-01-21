42d2e031ddd14d5c3513954c1c31ca65bcb317b3
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-01-21 22:36:25 +0530

    find products using findByPk()

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,24 +4,26 @@ const db=require('../util/database');
 
 
  exports.getProducts=((req,res,next)=>{
-    Product.fetchAll()
-    .then(([rows, fieldData])=>{
-        res.render('shop/product-list',
-            {prods: rows, 
+    Product.findAll()
+    .then(products=>{
+        res.render('shop/index',{
+            prods: products, 
             pageTitle:'All Products',
             path: '/products',
-            hasProducts: rows.length>0,
+            hasProducts: products.length>0,
             activeShop: true,
             productCSS: true
         });
     })
-    .catch((err)=>console.log(err));
+    .catch(err=>{
+        console.log(err);
+    }); 
 });
 
 exports.getProduct=((req,res,next)=>{
     const prodId=req.params.productId;
-    Product.findById(prodId)
-    .then(([product])=>{ 
+    Product.findByPk(prodId)
+    .then((product)=>{ 
         res.render('shop/product-detail',{product:product, pageTitle:product.title, path: '/products'});
     })
     .catch(err=>console.log(err))
@@ -71,16 +73,17 @@ exports.getCheckout=((req,res,next)=>{
 })
 
 exports.getIndex=((req,res,next)=>{
-    Product.fetchAll()
-    .then(([rows, fieldData])=>{
+    Product.findAll()
+    .then(products=>{
         res.render('shop/index',{
-            prods: rows,
+            prods: products,
             pageTitle:'Shop',
             path: '/'
         });
     })
-    .catch(err=>console.log(err));
-        
+    .catch(err=>{
+        console.log(err);
+    });      
 });
 
 exports.getOrders=((req,res,next)=>{
