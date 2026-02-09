c777b9d86dff9d7b849d3bcbc3d7b77e2593674e
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-02-10 01:21:31 +0530

    adding existing products and retreiving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,36 +76,43 @@ exports.getCart=((req,res,next)=>{
     // })
 })
 
-exports.postCart=((req,res,next)=>{
-    const prodId=req.body.productId;
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
+
     req.user
-    .getCart()
-    .then(cart=>{
-        fetchedCart=cart;
-        return cart.getProducts({where:{id:prodId}});
-    })
-    .then(products=>{
-        let product;
-        if(products.length>0){
-        const product=products[0];
-        }
-        let newQuantity = 1;
-        if(product){
-            //...
-        }
-        return Product.findByPk(prodId)
-        .then(product=>{
-            return fetchedCart.addProduct(product,{ 
-             through: { quantity : newQuantity } } )
+        .getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: prodId } });
         })
-        .then(()=>{
+        .then(products => {
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+            }
+
+            if (product) {
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product;
+            }
+
+            return Product.findByPk(prodId);
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, {
+                through: { quantity: newQuantity }
+            });
+        })
+        .then(() => {
             res.redirect('/cart');
         })
-        .catch(err=>console.log(err));
-    })
-    .catch(err=>console.log(err));
-})
+        .catch(err => console.log(err));
+};
+
+
 
 exports.postCartDeleteProduct = (req,res,next)=>{
     const prodId=req.body.productId;
@@ -113,6 +120,7 @@ exports.postCartDeleteProduct = (req,res,next)=>{
         Cart.deleteProduct(prodId, product.price);
         res.redirect('/cart');
     })
+
 };
 
 exports.getCheckout=((req,res,next)=>{
