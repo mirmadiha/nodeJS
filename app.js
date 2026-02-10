801db18b82b74cfad45b094ce77b9ead042c4da0
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-02-10 13:27:13 +0530

    adding n order model

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 
 const app=express();
@@ -50,6 +52,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);//optional same as above
 Cart.belongsToMany(Product, {through : CartItem});
 Product.belongsToMany(Cart, {through : CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through : OrderItem})
 
 
 sequelize.sync()
