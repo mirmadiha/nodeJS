36ee2ba3f2430e240685ede3d07ed45187c7482f
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-02-14 14:42:23 +0530

    fetching all products

--- models/product.js
+++ models/product.js
@@ -1,20 +1,34 @@
 const getDb = require('../util/database').getDb;
 
 class Product {
-    constructor(title,price,description, imageUrl){
-        this.title=title;
-        this.price=price;
-        this.description=description;
-        this.imageUrl=imageUrl;
+    constructor(title, price, description, imageUrl) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
     }
 
-    save(){
+    save() {
         const db = getDb();
-        db.collection('products').insertOne(this)
-        .then(result=>{
-            console.log(result);
-        })
-        .catch(err=>console.log(err));
+        return db.collection('products')
+            .insertOne(this)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => console.log(err));
+    }
+
+    static fetchAll() {
+        const db = getDb();
+        return db.collection('products').find().toArray()
+            .then(products => {
+                console.log(products);
+                return products;
+            })
+            .catch(err => console.log(err));
+        //find().toArray() returns a promise
+        //find() gets all products we can filter too if needed 
+        //it doesnt return immediately a promise but a cursor(an obkecct provided by mongo db)
     }
 };
 
@@ -45,4 +59,4 @@ class Product {
 //     },
 // })
 
-// module.exports = Product;
+module.exports = Product;
