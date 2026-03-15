3e7f05a937ad23b682a225bd6a1b5a13c3568da9
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-03-15 18:25:20 +0530

    getting orders

--- models/user.js
+++ models/user.js
@@ -106,6 +106,13 @@ class User {
             })
     }
 
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders')
+            .find({ 'user._id': new mongodb.ObjectId(this._id) })
+            .toArray()
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users').findOne({ _id: new mongodb.ObjectId(userId) })
