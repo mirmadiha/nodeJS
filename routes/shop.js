05e17d6068bc0080ffeb36c7a2ee5d8ab07f21ea
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-03-17 23:18:43 +0530

    connecitng to the mongodb server using mongoose

--- routes/shop.js
+++ routes/shop.js
@@ -7,20 +7,20 @@ const adminController = require('../controllers/admin')
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+// router.get('/', shopController.getIndex);
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-router.get('/products', shopController.getProducts);
+// router.get('/products', shopController.getProducts);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.post('/create-order', shopController.postOrder);
+// router.post('/create-order', shopController.postOrder);
 
-router.get('/products/:productId', shopController.getProduct);
+// router.get('/products/:productId', shopController.getProduct);
 
-router.get('/orders', shopController.getOrders);
+// router.get('/orders', shopController.getOrders);
 
 module.exports = router;
