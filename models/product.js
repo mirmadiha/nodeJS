5fd7fa243ef8b67c1629de783c9d13340b119993
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-03-18 18:53:34 +0530

    saving data through mongoose

--- models/product.js
+++ models/product.js
@@ -1,96 +1,122 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
-class Product {
-    constructor(title, price, description, imageUrl, id, userId) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this._id = id ? new mongodb.ObjectId(id) : null;
-        this.userId = userId;
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
     }
+});
 
-    save() {
-        const db = getDb();
-        let dbOp;
-        if (this._id) {
-            //update the product 
-            dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
-        }
-        else {
-            dbOp = db.collection('products').insertOne(this);
-        }
-        return dbOp
-            .then(result => {
-                console.log(result);
-            })
-            .catch(err => console.log(err));
-    }
-
-    static fetchAll() {
-        const db = getDb();
-        return db.collection('products').find().toArray()
-            .then(products => {
-                console.log(products);
-                return products;
-            })
-            .catch(err => console.log(err));
-        //find().toArray() returns a promise
-        //find() gets all products we can filter too if needed 
-        //it doesnt return immediately a promise but a cursor(an obkecct provided by mongo db)
-    }
-
-    static findById(prodId) {
-        console.log(prodId);
-        const db = getDb();
-        return db.collection('products').find({ _id: new mongodb.ObjectId(prodId) })
-            .next()
-            .then(product => {
-                console.log(product);
-                return product;
-            })
-            .catch(err => console.log(err));
-
-    }
-
-    static deleteById(prodId) {
-        const db = getDb();
-        return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
-            .then(result => {
-                console.log('deleted');
-            })
-            .catch(err => console.log(err));
-    }
+module.exports = mongoose.model('Product', productSchema);
 
 
-};
+
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
+
+// class Product {
+//     constructor(title, price, description, imageUrl, id, userId) {
+//         this.title = title;
+//         this.price = price;
+//         this.description = description;
+//         this.imageUrl = imageUrl;
+//         this._id = id ? new mongodb.ObjectId(id) : null;
+//         this.userId = userId;
+//     }
+
+//     save() {
+//         const db = getDb();
+//         let dbOp;
+//         if (this._id) {
+//             //update the product 
+//             dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+//         }
+//         else {
+//             dbOp = db.collection('products').insertOne(this);
+//         }
+//         return dbOp
+//             .then(result => {
+//                 console.log(result);
+//             })
+//             .catch(err => console.log(err));
+//     }
+
+//     static fetchAll() {
+//         const db = getDb();
+//         return db.collection('products').find().toArray()
+//             .then(products => {
+//                 console.log(products);
+//                 return products;
+//             })
+//             .catch(err => console.log(err));
+//         //find().toArray() returns a promise
+//         //find() gets all products we can filter too if needed 
+//         //it doesnt return immediately a promise but a cursor(an obkecct provided by mongo db)
+//     }
+
+//     static findById(prodId) {
+//         console.log(prodId);
+//         const db = getDb();
+//         return db.collection('products').find({ _id: new mongodb.ObjectId(prodId) })
+//             .next()
+//             .then(product => {
+//                 console.log(product);
+//                 return product;
+//             })
+//             .catch(err => console.log(err));
+
+//     }
+
+//     static deleteById(prodId) {
+//         const db = getDb();
+//         return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+//             .then(result => {
+//                 console.log('deleted');
+//             })
+//             .catch(err => console.log(err));
+//     }
 
 
-// const Sequelize = require('sequelize');
+// };
 
-// const sequelize = require('../util/database');
 
-// const Product = sequelize.define('product', {
-//     id: {
-//         type: Sequelize.INTEGER,
-//         autoIncrement: true,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     title: Sequelize.STRING,
-//     price: {
-//         type: Sequelize.DOUBLE,
-//         allowNull:false,
-//     },
-//     imageUrl: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     description: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-// })
+// // const Sequelize = require('sequelize');
 
-module.exports = Product;
+// // const sequelize = require('../util/database');
+
+// // const Product = sequelize.define('product', {
+// //     id: {
+// //         type: Sequelize.INTEGER,
+// //         autoIncrement: true,
+// //         allowNull: false,
+// //         primaryKey: true
+// //     },
+// //     title: Sequelize.STRING,
+// //     price: {
+// //         type: Sequelize.DOUBLE,
+// //         allowNull:false,
+// //     },
+// //     imageUrl: {
+// //         type: Sequelize.STRING,
+// //         allowNull: false
+// //     },
+// //     description: {
+// //         type: Sequelize.STRING,
+// //         allowNull: false
+// //     },
+// // })
+
+// module.exports = Product;
