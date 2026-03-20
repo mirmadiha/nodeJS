f0102cc082107003404948ccf0efed13ab74555b
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-03-20 10:23:07 +0530

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,15 +20,14 @@ exports.postEditProduct = ((req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
 
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-    );
-    product
-        .save()
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save()
+        })
         .then(result => {
             console.log("UPDATED RESULT !");
         })
@@ -40,8 +39,7 @@ exports.postEditProduct = ((req, res, next) => {
 })
 
 exports.getProducts = ((req, res, next) => {
-    Product.fetchAll()
-        // Product.findAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
