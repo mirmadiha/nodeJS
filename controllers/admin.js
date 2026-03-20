0608aabbb945d6be41e987f272de3debe1b7de6c
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-03-20 10:25:58 +0530

    deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -74,7 +74,7 @@ exports.postAddProduct = ((req, res, next) => {
 
 exports.postDeleteProduct = ((req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    Product.findByIdAndDelete(prodId)
         .then(() => {
             console.log("DESTROYED PRODUCT");
             res.redirect('/');
