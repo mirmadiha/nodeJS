f36287638b53128e1453c0f0766282eb68771706
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-04-05 19:53:48 +0530

    manipulating cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 exports.getLogin = ((req, res, next) => {
-    console.log(req.get('Cookie')
+    const isLoggedIn = (req.get('Cookie')
         .trim()
-        .split('=')[1]
+        .split('=')[1] === 'true'
     );
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     });
 })
 
