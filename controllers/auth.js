77be71ec347b4321f6f74e9c7fc59c46b6fe5738
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-04-06 21:46:49 +0530

    configuring cookie - secure

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ exports.getLogin = ((req, res, next) => {
 
 
 exports.postLogin = ((req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.setHeader('Set-Cookie', 'loggedIn=true; Secure'); //secure means this cookie can only be set if served on https
     res.redirect('/');
 })
 
