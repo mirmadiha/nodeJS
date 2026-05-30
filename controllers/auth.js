2e17859d8ce7391713b166d75eac6eb477afae7c
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-05-30 16:01:58 +0530

    creating token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -110,3 +111,55 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     });
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    }
+    else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        isAuthenticated: false,
+        errorMessage: message
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset')
+        }
+        const token = buffer.toString('hex');
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    req.flash('error', 'No user found with that email!')
+                    return res.redirect('/reset')
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;  //1 hour in milliseconds
+                return user.save()
+            })
+            .then(result => {
+                res.redirect('/');
+                return transporter.sendMail({
+                    to: req.body.email,
+                    from: 'aijazmirmadiha@gmail.com',
+                    subject: 'Reset Password',
+                    html: `
+                <p>You requested a password reset</p>
+                <p>Click this link to reset your password:</p>
+                <a href="http://localhost:4000/reset-password/${token}">Reset Password</a>
+                `
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+}
