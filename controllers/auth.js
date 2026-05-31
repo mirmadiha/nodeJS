709d6324f116f23052daf824311f8dd1c0de9d7e
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-05-31 13:06:26 +0530

    fix: store SendGrid API key in .env instead of hardcoding

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: 'REMOVED'
+        api_key: process.env.SENDGRID_API_KEY
     }
 }))
 
