8108b2ac730e329319476bbf4ed7fff068b125ad
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-06-10 23:54:47 +0530

    handling errors

--- app.js
+++ app.js
@@ -47,8 +47,14 @@ app.use(
 
 app.use(csrfProtection);
 app.use(flash());
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use((req, res, next) => {
+
     if (!req.session.user) {
         return next();
     }
@@ -61,14 +67,8 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err =>{
-            throw new Error(err);
+            next (new Error(err));
         });
-})
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
 })
 
 app.use("/admin", adminRoutes);
@@ -80,7 +80,12 @@ app.get('/500', errorControllers.get500);
 app.use(errorControllers.get404);
 
 app.use((error, req, res, next)=>{
-    res.redirect('/500');
+    res.status(500).render("500", {
+        pageTitle: 'Error',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+        csrfToken: req.csrfToken()
+    });
 })
 
 mongoose.connect(
