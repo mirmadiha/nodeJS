2007e994cad2f624dbe67f8b5817d90977dc9e40
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2026-06-18 00:42:55 +0530

    adding pagination logic on the server

--- controllers/shop.js
+++ controllers/shop.js
@@ -185,15 +185,26 @@ exports.postOrder = (req, res, next) => {
 
 exports.getIndex = ((req, res, next) => {
     const page = req.query.page;
-    Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE)
+    let totalItems;
+
+    Product.find().countDocuments().then(numProducts => {
+        totalItems = numProducts
+        return Product.find()
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE)
+    })
         .then(products => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
                 path: '/',
-
+                totalItems: totalItems,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPrevPage: page > 1,
+                nextPage: page + 1,
+                prevPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
